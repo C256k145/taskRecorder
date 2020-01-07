63eb30bfc3f799fa37c25f8d0af0d5537c48eb66
c256k145 <noremackientz@gmail.com>
2020-01-07 17:27:09 -0600

    initial commit

---
+++ taskRecorder.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	remote = "cameron:linux@tcp(10.0.0.104:3306)/work_records"
+	local  = "cameron:linux@tcp(127.0.0.0:3306)/work_records"
+)
+
+func main() {
+	db, err := sql.Open("mysql",
+		remote)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	name, description, time, difficulty := getInput()
+	query := "INSERT INTO tasks (name, description, time_taken, difficulty) VALUES (\"" + name + "\",\"" + description + "\",\"" + time + "\",\"" + difficulty + "\");"
+	fmt.Println("Query entered: \n	", query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.Exec(query)
+	defer db.Close()
+}
+
+func getInput() (string, string, string, string) {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Name of Task: ")
+	name, _ := reader.ReadString('\n')
+	name = strings.TrimSuffix(name, "\n")
+
+	fmt.Print("Description: ")
+	description, _ := reader.ReadString('\n')
+	description = strings.TrimSuffix(description, "\n")
+
+	fmt.Print("Time Taken: ")
+	time, _ := reader.ReadString('\n')
+	time = strings.TrimSuffix(time, "\n")
+
+	fmt.Print("Difficulty: ")
+	difficulty, _ := reader.ReadString('\n')
+	difficulty = strings.TrimSuffix(difficulty, "\n")
+
+	return name, description, time, difficulty
+}
